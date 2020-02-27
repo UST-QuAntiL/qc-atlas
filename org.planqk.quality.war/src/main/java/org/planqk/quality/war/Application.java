238e7cbd54129a6a2bf44ae5d55d070201e3db9e
wederbn <benjamin.weder@iaas.uni-stuttgart.de>
2020-02-27 08:52:13 +0100

    Move application file to separate module

--- org.planqk.quality.war/src/main/java/org/planqk/quality/war/Application.java
+++ org.planqk.quality.war/src/main/java/org/planqk/quality/war/Application.java
@@ -14,11 +14,10 @@
  *  ******************************************************************************
  */
 
-package org.planqk.quality.api;
+package org.planqk.quality.war;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
