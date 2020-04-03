6225a81b868b5e31144c68bd9bc489a119611428
Manuela Weigold <manuela.weigold@iaas.uni-stuttgart.de>
2020-04-03 10:09:29 +0200

    create first real controller test

--- org.planqk.atlas.web/src/test/org/planqk/atlas/web/controller/TagControllerTest.java
+++ org.planqk.atlas.web/src/test/org/planqk/atlas/web/controller/TagControllerTest.java
@@ -24,28 +24,30 @@ import java.util.List;
 
 import org.planqk.atlas.core.model.Tag;
 import org.planqk.atlas.core.services.TagService;
+import org.planqk.atlas.web.Constants;
 
-import org.aspectj.lang.annotation.Before;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -58,34 +60,34 @@ public class TagControllerTest {
 
     private MockMvc mockMvc;
 
+    @Before
+    public void before() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(tagController).build();
+    }
+
+    @Test
+    public void contextLoaded() throws Exception {
+        assertThat(tagController).isNotNull();
+    }
+
     @Test
     public void testList() throws Exception {
-        MockitoAnnotations.initMocks(this);
-        mockMvc = MockMvcBuilders.standaloneSetup(tagController).build();
         List<Tag> tags = new ArrayList<>();
-        tags.add(new Tag());
+        Tag tag1 = new Tag();
+        tag1.setKey("testkey");
+        tag1.setValue("testvalue");
+        tags.add(tag1);
         tags.add(new Tag());
         Pageable pageable = PageRequest.of(0, 2);
 
-        when(tagService.findAll(pageable)).thenReturn(null);
-        mockMvc.perform(get("atlas/tags").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
-    }
-    @Test
-    public void testList2() throws Exception {
-        MockitoAnnotations.initMocks(this);
-        mockMvc = MockMvcBuilders.standaloneSetup(tagController).build();
-        List<Tag> tags = new ArrayList<>();
-        tags.add(new Tag());
-        tags.add(new Tag());
-        Pageable pageable = PageRequest.of(0, 2);
+        Page<Tag> page = new PageImpl<Tag>(tags, pageable, tags.size());
+        when(tagService.findAll(any(Pageable.class))).thenReturn(page);
 
-        when(tagService.findAll(pageable)).thenReturn(null);
-        mockMvc.perform(get("tags").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
-    }
-
-    private Page<Tag> constructPage(List<Tag> tags, Pageable pageable) {
-        long start = pageable.getOffset();
-        long end = (start + pageable.getPageSize()) > tags.size() ? tags.size() : (start + pageable.getPageSize());
-        return new PageImpl<Tag>(tags.subList((int) start, (int) end), pageable, tags.size());
+        mockMvc.perform(get("/" + Constants.TAGS + "/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content", hasSize(2)))
+                .andExpect(jsonPath("$.content[0].key", is(tag1.getKey())))
+                .andDo(print());
     }
 }
