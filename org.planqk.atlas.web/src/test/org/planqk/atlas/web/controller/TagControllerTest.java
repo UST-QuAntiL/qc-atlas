0bea81bfa5fb8a57cfc3ea867b2ac9c3ba0fbba7
Manuela Weigold <manuela.weigold@iaas.uni-stuttgart.de>
2020-04-03 10:12:00 +0200

    use lighter test setup annotations

--- org.planqk.atlas.web/src/test/org/planqk/atlas/web/controller/TagControllerTest.java
+++ org.planqk.atlas.web/src/test/org/planqk/atlas/web/controller/TagControllerTest.java
@@ -28,15 +28,17 @@ import org.planqk.atlas.web.Constants;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -50,7 +52,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(TagController.class)
 public class TagControllerTest {
     @Mock
     private TagService tagService;
