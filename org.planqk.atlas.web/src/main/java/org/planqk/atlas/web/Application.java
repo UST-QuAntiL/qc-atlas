cc931a5810ae7b411f250b222c20937bd2d85b5b
Tim Niederhausen <st144339@stud.uni-stuttgart.de>
2020-05-20 12:00:58 +0200

    Add main method to Application class
    Also set the correct context-path for the embedded tomcat server.

--- org.planqk.atlas.web/src/main/java/org/planqk/atlas/web/Application.java
+++ org.planqk.atlas.web/src/main/java/org/planqk/atlas/web/Application.java
@@ -21,6 +21,7 @@ package org.planqk.atlas.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -44,5 +45,15 @@ public class Application extends SpringBootServletInitializer {
                 "ATLAS IS READY TO USE!\n" +
                 "===================================================";
         LOG.info(readyMessage);
+    }
+
+    /**
+     * Launch the embedded Tomcat server.
+     *
+     * See `application.properties` for its configuration.
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
 }
