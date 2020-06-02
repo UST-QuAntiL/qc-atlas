01070d218747e26d2e9dc2173b1689de6d6242b4
Stefan Basaric <you@example.com>
2020-06-02 09:55:33 +0200

    Adjusted constants values

--- org.planqk.atlas.web/src/main/java/org/planqk/atlas/web/Constants.java
+++ org.planqk.atlas.web/src/main/java/org/planqk/atlas/web/Constants.java
@@ -30,9 +30,9 @@ public class Constants {
     public static final String PROVIDERS = "providers";
     public static final String QPUS = "qpus";
     public static final String TAGS = "tags";
-    public static final String PROBLEM_TYPES = "problemtypes";
-    public static final String ALGORITHM_RELATIONS = "algorithmrelations";
-    public static final String ALGO_RELATION_TYPES = "algorelationtypes";
+    public static final String PROBLEM_TYPES = "problem-types";
+    public static final String ALGORITHM_RELATIONS = "algorithm-relations";
+    public static final String ALGO_RELATION_TYPES = "algo-relation-types";
 
     // link names
     public static final String ALGORITHM_LINK = "implementedAlgorithm";
