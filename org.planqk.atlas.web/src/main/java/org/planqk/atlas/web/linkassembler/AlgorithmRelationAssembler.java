0349865d9c4ca1e4697a2a1d2d2f68d2f7f8bfc6
TGrosskopf <38653495+TGrosskopf@users.noreply.github.com>
2020-06-07 18:15:15 +0200

    add links to AlgorithmRelationDto

--- org.planqk.atlas.web/src/main/java/org/planqk/atlas/web/linkassembler/AlgorithmRelationAssembler.java
+++ org.planqk.atlas.web/src/main/java/org/planqk/atlas/web/linkassembler/AlgorithmRelationAssembler.java
@@ -1,5 +1,12 @@
 package org.planqk.atlas.web.linkassembler;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.util.UUID;
+
+import org.planqk.atlas.web.controller.AlgoRelationTypeController;
+import org.planqk.atlas.web.controller.AlgorithmController;
 import org.planqk.atlas.web.dtos.AlgorithmRelationDto;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.stereotype.Component;
@@ -9,7 +16,21 @@ public class AlgorithmRelationAssembler extends GenericLinkAssembler<AlgorithmRe
 
     @Override
     public void addLinks(EntityModel<AlgorithmRelationDto> resource) {
-        // TODO: Add links
+        resource.add(linkTo(methodOn(AlgorithmController.class).getAlgorithm(getSourceAlgorithmId(resource))).withRel("sourceAlgorithm"));
+        resource.add(linkTo(methodOn(AlgorithmController.class).getAlgorithm(getTargetAlgorithmId(resource))).withRel("targetAlgorithm"));
+        resource.add(linkTo(methodOn(AlgoRelationTypeController.class).getAlgoRelationTypeById(getAlgoRelationTypeId(resource))).withRel("algoRelationType"));
+    }
+
+    private UUID getSourceAlgorithmId(EntityModel<AlgorithmRelationDto> resource) {
+        return resource.getContent().getSourceAlgorithm().getId();
+    }
+
+    private UUID getTargetAlgorithmId(EntityModel<AlgorithmRelationDto> resource) {
+        return resource.getContent().getTargetAlgorithm().getId();
+    }
+
+    private UUID getAlgoRelationTypeId(EntityModel<AlgorithmRelationDto> resource) {
+        return resource.getContent().getAlgoRelationType().getId();
     }
 
 }
