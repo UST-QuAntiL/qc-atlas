ae0674820be1308b1903ffeb75bb4a4e1aceaebe
Tim Niederhausen <st144339@stud.uni-stuttgart.de>
2020-06-07 13:09:50 +0200

    Remove duplicate JPA configuration
    We can't have two classes using @EnableJpaRepositories with the same package(s)

--- org.planqk.atlas.web/src/main/java/org/planqk/atlas/web/Application.java
+++ org.planqk.atlas.web/src/main/java/org/planqk/atlas/web/Application.java
@@ -29,7 +29,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
