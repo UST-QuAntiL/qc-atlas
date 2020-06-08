0bd1add446e2acb9e3786287405b4615f7302102
Stefan Basaric <you@example.com>
2020-06-08 07:54:55 +0200

    Format Code

--- org.planqk.atlas.web/src/main/java/org/planqk/atlas/web/utils/ModelMapperUtils.java
+++ org.planqk.atlas.web/src/main/java/org/planqk/atlas/web/utils/ModelMapperUtils.java
@@ -14,36 +14,36 @@ import org.springframework.data.domain.Page;
 
 public class ModelMapperUtils {
 
-    private static ModelMapper mapper = initModelMapper();
+	private static ModelMapper mapper = initModelMapper();
 
-    public static <D, T> Set<D> convertSet(Set<T> entities, Class<D> dtoClass) {
-        Set<D> resultSet = new HashSet<D>();
-        for (T entity : entities) {
-            resultSet.add(convert(entity, dtoClass));
-        }
-        return resultSet;
-    }
+	public static <D, T> Set<D> convertSet(Set<T> entities, Class<D> dtoClass) {
+		Set<D> resultSet = new HashSet<D>();
+		for (T entity : entities) {
+			resultSet.add(convert(entity, dtoClass));
+		}
+		return resultSet;
+	}
 
-    public static <D, T> Page<D> convertPage(Page<T> entities, Class<D> dtoClass) {
-        return entities.map(objectEntity -> convert(objectEntity, dtoClass));
-    }
+	public static <D, T> Page<D> convertPage(Page<T> entities, Class<D> dtoClass) {
+		return entities.map(objectEntity -> convert(objectEntity, dtoClass));
+	}
 
-	public static <D, T> D convert(final T entity, Class<D> outClass) {	
-        return mapper.map(entity, outClass);
-    }
-    
-    private static ModelMapper initModelMapper() {
-    	ModelMapper mapper = new ModelMapper();
-    	// Config ModelMapper to always use child classes of Algorithm and it's DTO
-    	mapper.createTypeMap(ClassicAlgorithm.class, AlgorithmDto.class)
-        .setConverter(mappingContext -> mapper.map(mappingContext.getSource(), ClassicAlgorithmDto.class));
-        mapper.createTypeMap(QuantumAlgorithm.class, AlgorithmDto.class)
-        .setConverter(mappingContext -> mapper.map(mappingContext.getSource(), QuantumAlgorithmDto.class));
-        mapper.createTypeMap(ClassicAlgorithmDto.class, Algorithm.class)
-        .setConverter(mappingContext -> mapper.map(mappingContext.getSource(), ClassicAlgorithm.class));
-        mapper.createTypeMap(QuantumAlgorithmDto.class, Algorithm.class)
-        .setConverter(mappingContext -> mapper.map(mappingContext.getSource(), QuantumAlgorithm.class));
-    	
-    	return mapper;
-    }
+	public static <D, T> D convert(final T entity, Class<D> outClass) {
+		return mapper.map(entity, outClass);
+	}
+
+	private static ModelMapper initModelMapper() {
+		ModelMapper mapper = new ModelMapper();
+		// Config ModelMapper to always use child classes of Algorithm and it's DTO
+		mapper.createTypeMap(ClassicAlgorithm.class, AlgorithmDto.class)
+				.setConverter(mappingContext -> mapper.map(mappingContext.getSource(), ClassicAlgorithmDto.class));
+		mapper.createTypeMap(QuantumAlgorithm.class, AlgorithmDto.class)
+				.setConverter(mappingContext -> mapper.map(mappingContext.getSource(), QuantumAlgorithmDto.class));
+		mapper.createTypeMap(ClassicAlgorithmDto.class, Algorithm.class)
+				.setConverter(mappingContext -> mapper.map(mappingContext.getSource(), ClassicAlgorithm.class));
+		mapper.createTypeMap(QuantumAlgorithmDto.class, Algorithm.class)
+				.setConverter(mappingContext -> mapper.map(mappingContext.getSource(), QuantumAlgorithm.class));
+
+		return mapper;
+	}
 }
