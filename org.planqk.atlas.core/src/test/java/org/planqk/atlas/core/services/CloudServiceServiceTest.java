87a1a7d054cac25d1711ae9d313169dfb97e6090
Tobias Haller <tobias_haller@outlook.de>
2020-06-09 16:21:49 +0200

    Implemented cloud service test

--- org.planqk.atlas.core/src/test/java/org/planqk/atlas/core/services/CloudServiceServiceTest.java
+++ org.planqk.atlas.core/src/test/java/org/planqk/atlas/core/services/CloudServiceServiceTest.java
@@ -1,9 +1,18 @@
 package org.planqk.atlas.core.services;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.planqk.atlas.core.model.CloudService;
 import org.planqk.atlas.core.repository.CloudServiceRepository;
 import org.planqk.atlas.core.util.AtlasDatabaseTestBase;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CloudServiceServiceTest extends AtlasDatabaseTestBase {
 
@@ -14,8 +23,11 @@ public class CloudServiceServiceTest extends AtlasDatabaseTestBase {
     private CloudServiceRepository cloudServiceRepository;
 
     @Test
-    void testAddCloudService_WithoutBackends() {
+    void testAddCloudService_WithoutBackends() throws MalformedURLException {
+        CloudService cloudService = getGenericTestCloudServiceWithoutRelations("testCloudService");
 
+        var storedCloudService = cloudServiceService.save(cloudService);
+        assertCloudServiceEquality(storedCloudService, cloudService);
     }
 
     @Test
@@ -25,21 +37,55 @@ public class CloudServiceServiceTest extends AtlasDatabaseTestBase {
 
     @Test
     void testFindCloudServiceById_ElementNotFound() {
-
+        Assertions.assertThrows(NoSuchElementException.class, () -> {
+            cloudServiceService.findById(UUID.randomUUID());
+        });
     }
 
     @Test
-    void testFindCloudServiceById_ElementFound() {
+    void testFindCloudServiceById_ElementFound() throws MalformedURLException {
+        CloudService cloudService = getGenericTestCloudServiceWithoutRelations("testCloudService");
 
+        var storedCloudService = cloudServiceService.save(cloudService);
+
+        cloudServiceService.findById(storedCloudService.getId());
+
+        assertCloudServiceEquality(storedCloudService, cloudService);
     }
 
     @Test
-    void testDeleteCloudService_WithoutBackends() {
+    void testDeleteCloudService_WithoutBackends() throws MalformedURLException {
+        CloudService cloudService = getGenericTestCloudServiceWithoutRelations("testCloudService");
 
+        var storedCloudService = cloudServiceService.save(cloudService);
+        cloudServiceService.findById(storedCloudService.getId());
+
+        cloudServiceService.delete(storedCloudService.getId());
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> {
+            cloudServiceService.findById(storedCloudService.getId());
+        });
     }
 
     @Test
     void testDeleteSoftwarePlatform_WithBackends() {
         // TODO: When backend is implemented
+    }
+
+    private void assertCloudServiceEquality(CloudService dbCloudService, CloudService compareCloudService) {
+        assertThat(dbCloudService.getId()).isNotNull();
+        assertThat(dbCloudService.getName()).isEqualTo(compareCloudService.getName());
+        assertThat(dbCloudService.getProvider()).isEqualTo(compareCloudService.getProvider());
+        assertThat(dbCloudService.getUrl()).isEqualTo(compareCloudService.getUrl());
+        assertThat(dbCloudService.getCostModel()).isEqualTo(compareCloudService.getCostModel());
+    }
+
+    private CloudService getGenericTestCloudServiceWithoutRelations(String name) throws MalformedURLException {
+        CloudService cloudService = new CloudService();
+        cloudService.setName(name);
+        cloudService.setProvider("testProvider");
+        cloudService.setUrl(new URL("http://example.com"));
+        cloudService.setCostModel("testCostModel");
+        return cloudService;
     }
 }
