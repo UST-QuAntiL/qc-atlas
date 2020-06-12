cb6364ee7d751ccb6153222823c0f178295f9c55
Felix Burk <felix.burk@googlemail.com>
2020-06-12 13:54:46 +0200

    revert some changes in RootController but adds comment on return type

--- org.planqk.atlas.web/src/main/java/org/planqk/atlas/web/controller/RootController.java
+++ org.planqk.atlas.web/src/main/java/org/planqk/atlas/web/controller/RootController.java
@@ -19,17 +19,10 @@
 
 package org.planqk.atlas.web.controller;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.planqk.atlas.web.Constants;
 
-import org.planqk.atlas.web.utils.HateoasUtils;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.Link;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
@@ -52,12 +45,10 @@ public class RootController {
 
     @Operation(responses = {@ApiResponse(responseCode = "200")})
     @GetMapping("/")
-    public HttpEntity<RepresentationModel> root() {
+    public HttpEntity<RepresentationModel<?>> root() {
         // EntityModel can unfortunately not be used here because it requires non null content
-
-        // raw use of parameterized class is best practice according the official hateoas examples
-        // see: https://github.com/spring-projects/spring-hateoas-examples/blob/master/hypermedia/src/main/java/org/springframework/hateoas/examples/RootController.java
-        RepresentationModel responseEntity = new RepresentationModel<>();
+        // OpenAPI does not care about ? in this case because we only provide links anyway
+        RepresentationModel<?> responseEntity = new RepresentationModel<>();
 
         // add links to sub-controllers
         responseEntity.add(linkTo(methodOn(RootController.class).root()).withSelfRel());
@@ -70,6 +61,6 @@ public class RootController {
                 methodOn(PublicationController.class).getPublications(Constants.DEFAULT_PAGE_NUMBER, Constants.DEFAULT_PAGE_SIZE))
                 .withRel(Constants.PUBLICATIONS));
 
-        return ResponseEntity.ok(responseEntity);
+        return new ResponseEntity<>(responseEntity, HttpStatus.OK);
     }
 }
