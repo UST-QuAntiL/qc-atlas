7ec3d77f2ba1839a202c17185e3bab22dc3361ac
Christian Müller <dev@c-mueller.xyz>
2020-06-13 17:02:04 +0200

    Implement TagService Test

--- org.planqk.atlas.core/src/test/java/org/planqk/atlas/core/services/TagServiceTest.java
+++ org.planqk.atlas.core/src/test/java/org/planqk/atlas/core/services/TagServiceTest.java
@@ -1,36 +1,112 @@
 package org.planqk.atlas.core.services;
 
-import org.junit.jupiter.api.Test;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+import org.planqk.atlas.core.model.Tag;
 import org.planqk.atlas.core.repository.TagRepository;
 import org.planqk.atlas.core.util.AtlasDatabaseTestBase;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TagServiceTest extends AtlasDatabaseTestBase {
 
     @Autowired
-    TagService tagService;
+    private TagService tagService;
 
     @Autowired
     private TagRepository tagRepository;
 
     @Test
     void testAddTag() {
+        var tag = new Tag();
+        tag.setValue("Hello");
+        tag.setKey("World");
+        tag = tagService.save(tag);
 
+        var dbTag = tagRepository.findById(tag.getId());
+        assertThat(dbTag).isEqualTo(tag);
     }
 
     @Test
     void testFindTagById_ElementNotFound() {
-
+        assertThrows(NoSuchElementException.class, () -> {
+            tagService.getTagById(UUID.randomUUID());
+        });
     }
 
     @Test
     void testFindTagById_ElementFound() {
+        var tag = new Tag();
+        tag.setValue("Hello");
+        tag.setKey("World");
+        tag = tagService.save(tag);
 
+        var dbTag = tagService.getTagById(tag.getId());
+        assertThat(dbTag).isEqualTo(tag);
     }
 
     @Test
-    void testDeleteTag() {
-
+    void testAddOrUpdateMany_AddAll() {
+        var tags = new HashSet<Tag>();
+        for (int i = 0; i < 10; i++) {
+            var tag = new Tag();
+            tag.setValue("Hello " + i);
+            tag.setKey("World " + i);
+            tags.add(tag);
+        }
+        var updatedSet = tagService.createOrUpdateAll(tags);
+        assertTagListIdsPresent(updatedSet, tags);
+        assertTagListEquality(updatedSet, tags);
     }
 
+    @Test
+    void testAddOrUpdateMany_AddHalf_UpdateHalf() {
+        var tags = new HashSet<Tag>();
+        for (int i = 0; i < 10; i++) {
+            var tag = new Tag();
+            tag.setValue("Hello " + i);
+            tag.setKey("World " + i);
+            if (i % 2 == 0) {
+                tags.add(tag);
+            } else {
+                tags.add(tagService.save(tag));
+            }
+        }
+        var updatedSet = tagService.createOrUpdateAll(tags);
+        assertTagListIdsPresent(updatedSet, tags);
+        assertTagListEquality(updatedSet, tags);
+    }
+
+    @Test
+    void testFindAll() {
+        var tags = new HashSet<Tag>();
+        for (int i = 0; i < 10; i++) {
+            var tag = new Tag();
+            tag.setValue("Hello " + i);
+            tag.setKey("World " + i);
+            tags.add(tag);
+        }
+        var updatedSet = tagService.createOrUpdateAll(tags);
+        var dbOutput = tagService.findAll(Pageable.unpaged()).getContent();
+        assertTagListIdsPresent(updatedSet, dbOutput);
+        assertTagListEquality(updatedSet, dbOutput);
+    }
+
+    private void assertTagListIdsPresent(Collection<Tag> a, Collection<Tag> b) {
+        assertThat(a.stream().filter(e -> e.getId() != null)).isEqualTo(b.size());
+    }
+
+    private void assertTagListEquality(Collection<Tag> a, Collection<Tag> b) {
+        assertThat(a.size()).isEqualTo(b.size());
+        assertThat(a.stream().filter(e -> b.stream().anyMatch(el -> e.getKey().equals(el.getKey()))))
+                .isEqualTo(a.size());
+    }
 }
