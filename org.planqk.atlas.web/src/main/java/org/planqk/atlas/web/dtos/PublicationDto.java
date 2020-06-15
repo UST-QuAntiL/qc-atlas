d68d58895420ad323551be2c806a7103c782a042
Valentin Seifermann <valesayfa@t-online.de>
2020-06-15 03:31:36 +0200

    fixed URL mapping error

--- org.planqk.atlas.web/src/main/java/org/planqk/atlas/web/dtos/PublicationDto.java
+++ org.planqk.atlas.web/src/main/java/org/planqk/atlas/web/dtos/PublicationDto.java
@@ -3,16 +3,18 @@ package org.planqk.atlas.web.dtos;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.URL;
+
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import java.net.URL;
+
 import java.util.List;
 import java.util.UUID;
 
 @EqualsAndHashCode
 @Data
 @NoArgsConstructor
-public class PublicationDto{
+public class PublicationDto {
 
     private UUID id;
 
@@ -21,9 +23,9 @@ public class PublicationDto{
 
     private String doi;
 
-    private URL url;
+    @URL(message = "Publication URL must be a valid URL!")
+    private String url;
 
     @NotEmpty(message = "Authors of the Publication must not be empty!")
     private List<String> authors;
-
 }
