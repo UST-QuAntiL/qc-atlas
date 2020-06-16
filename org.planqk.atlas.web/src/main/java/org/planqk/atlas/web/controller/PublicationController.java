7268d641cf044cbe6be1a3bffda7e3d55fbce32e
Valentin Seifermann <valesayfa@t-online.de>
2020-06-16 02:34:15 +0200

    minor fixe + added missing license headers + formatting

--- org.planqk.atlas.web/src/main/java/org/planqk/atlas/web/controller/PublicationController.java
+++ org.planqk.atlas.web/src/main/java/org/planqk/atlas/web/controller/PublicationController.java
@@ -1,9 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2020 University of Stuttgart
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package org.planqk.atlas.web.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.planqk.atlas.core.model.Algorithm;
 import org.planqk.atlas.core.model.Publication;
 import org.planqk.atlas.core.services.PublicationService;
@@ -15,6 +34,7 @@ import org.planqk.atlas.web.linkassembler.PublicationAssembler;
 import org.planqk.atlas.web.utils.HateoasUtils;
 import org.planqk.atlas.web.utils.ModelMapperUtils;
 import org.planqk.atlas.web.utils.RestUtils;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedResourcesAssembler;
