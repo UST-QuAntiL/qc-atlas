dd8d9402940fb49f62a75c04c4348b35ad885368
Christian Müller <dev@c-mueller.xyz>
2020-07-08 14:55:17 +0200

    Update Global CORS policy (#106)

---
+++ org.planqk.atlas.web/src/main/java/org/planqk/atlas/web/CorsConfiguration.java
@@ -0,0 +1,15 @@
+package org.planqk.atlas.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS");
+    }
+
+}
