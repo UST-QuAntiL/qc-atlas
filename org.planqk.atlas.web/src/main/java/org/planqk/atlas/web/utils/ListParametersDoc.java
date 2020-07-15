d50e3960bb3424bfeaa79d2180b3b0170dbd89b5
Tim Niederhausen <st144339@stud.uni-stuttgart.de>
2020-07-15 09:53:28 +0200

    Add class wrapping common data-list parameters (paging, sorting, search query)

---
+++ org.planqk.atlas.web/src/main/java/org/planqk/atlas/web/utils/ListParametersDoc.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2020 University of Stuttgart
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package org.planqk.atlas.web.utils;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springdoc.data.rest.converters.PageableAsQueryParam;
+
+@PageableAsQueryParam
+@Parameter(in = ParameterIn.QUERY, description = "Filter criteria for this query ", name = "search",
+        content = @Content(schema = @Schema(type = "string")))
+public @interface ListParametersDoc {
+}
