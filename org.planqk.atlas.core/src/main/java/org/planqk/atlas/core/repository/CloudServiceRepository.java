0b7f494b86051712903395b63718bc9efd09ccc1
Christian Müller <dev@c-mueller.xyz>
2020-07-21 16:37:29 +0200

    Fix import

--- org.planqk.atlas.core/src/main/java/org/planqk/atlas/core/repository/CloudServiceRepository.java
+++ org.planqk.atlas.core/src/main/java/org/planqk/atlas/core/repository/CloudServiceRepository.java
@@ -1,11 +1,11 @@
 package org.planqk.atlas.core.repository;
 
-import java.awt.print.Pageable;
 import java.util.UUID;
 
 import org.planqk.atlas.core.model.CloudService;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
