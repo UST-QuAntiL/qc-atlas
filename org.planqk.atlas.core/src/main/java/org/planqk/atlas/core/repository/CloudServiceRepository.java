24d0a0289429305380d2df4ed128756440982838
Christian Müller <dev@c-mueller.xyz>
2020-07-21 16:34:56 +0200

    Add Query for CloudService -> SoftwarePlatform

--- org.planqk.atlas.core/src/main/java/org/planqk/atlas/core/repository/CloudServiceRepository.java
+++ org.planqk.atlas.core/src/main/java/org/planqk/atlas/core/repository/CloudServiceRepository.java
@@ -1,9 +1,11 @@
 package org.planqk.atlas.core.repository;
 
+import java.awt.print.Pageable;
 import java.util.UUID;
 
 import org.planqk.atlas.core.model.CloudService;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,6 +14,9 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(exported = false)
 public interface CloudServiceRepository extends JpaRepository<CloudService, UUID> {
     boolean existsCloudServiceById(UUID id);
+
+    @Query("SELECT cs FROM CloudService cs JOIN cs.softwarePlatforms sp WHERE sp.id = :spid")
+    Page<CloudService> findCloudServicesBySoftwarePlatformId(@Param("spid") UUID id, Pageable p);
 
     @Query("SELECT COUNT(cs) " +
             "FROM CloudService cs " +
