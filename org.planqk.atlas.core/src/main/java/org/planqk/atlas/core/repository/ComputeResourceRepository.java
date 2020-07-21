397384b70b38f6e5aefa44807e2df1193d40f3c7
Christian Müller <dev@c-mueller.xyz>
2020-07-21 15:56:02 +0200

    Add Queries

--- org.planqk.atlas.core/src/main/java/org/planqk/atlas/core/repository/ComputeResourceRepository.java
+++ org.planqk.atlas.core/src/main/java/org/planqk/atlas/core/repository/ComputeResourceRepository.java
@@ -5,8 +5,18 @@ import java.util.UUID;
 
 import org.planqk.atlas.core.model.ComputeResource;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ComputeResourceRepository extends JpaRepository<ComputeResource, UUID> {
+    @Query("SELECT CloudService FROM CloudService cs JOIN cs.providedComputeResources res WHERE res.id = :csid")
+    Page<ComputeResource> findComputeResourceByCloudServiceId(@Param("csid") UUID csid, Pageable p);
+
+    @Query("SELECT SoftwarePlatform FROM SoftwarePlatform sp JOIN sp.supportedComputeResources res WHERE res.id = :csid")
+    Page<ComputeResource> findComputeResourceBySoftwarePlatformId(@Param("spid") UUID spid, Pageable p);
+
     Set<ComputeResource> findByName(String name);
 }
