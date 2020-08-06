d5e22af5393bdab01959a8a9f99487927a251c36
radufalcusanStoneone <radu.falcusan@stoneone.de>
2020-08-06 15:36:26 +0200

    upload-sketch: KnowledgeArtifact - add creationDate and lastModifiedAt attributes

--- org.planqk.atlas.core/src/main/java/org/planqk/atlas/core/model/KnowledgeArtifact.java
+++ org.planqk.atlas.core/src/main/java/org/planqk/atlas/core/model/KnowledgeArtifact.java
@@ -1,5 +1,8 @@
 package org.planqk.atlas.core.model;
 
+import java.util.Date;
+
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
@@ -8,5 +11,11 @@ import javax.persistence.MappedSuperclass;
 @EqualsAndHashCode(callSuper = true)
 @MappedSuperclass
 @NoArgsConstructor
+@Data
 public class KnowledgeArtifact extends HasId {
+
+    public Date creationDate;
+
+    public Date lastModifiedAt;
+
 }
