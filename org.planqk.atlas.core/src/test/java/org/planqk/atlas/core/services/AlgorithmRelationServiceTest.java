eb1bd02d55705d80eb43ff395cd02b0184572a62
Tobias Haller <tobias_haller@outlook.de>
2020-08-28 23:53:48 +0200

    Implement AlgorithmRelationService tests

--- org.planqk.atlas.core/src/test/java/org/planqk/atlas/core/services/AlgorithmRelationServiceTest.java
+++ org.planqk.atlas.core/src/test/java/org/planqk/atlas/core/services/AlgorithmRelationServiceTest.java
@@ -19,11 +19,21 @@
 
 package org.planqk.atlas.core.services;
 
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+import org.planqk.atlas.core.model.Algorithm;
+import org.planqk.atlas.core.model.AlgorithmRelation;
+import org.planqk.atlas.core.model.AlgorithmRelationType;
+import org.planqk.atlas.core.model.ClassicAlgorithm;
 import org.planqk.atlas.core.util.AtlasDatabaseTestBase;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 public class AlgorithmRelationServiceTest extends AtlasDatabaseTestBase {
@@ -36,47 +46,161 @@ public class AlgorithmRelationServiceTest extends AtlasDatabaseTestBase {
     private AlgorithmService algorithmService;
 
     @Test
-    void createAlgorithmRelation_TypeFound() {
+    void createAlgorithmRelation() {
+        Algorithm sourceAlgorithm = getCreatedAlgorithm("sourceAlgorithmName");
+        Algorithm targetAlgorithm = getCreatedAlgorithm("targetAlgorithmName");
 
-    }
+        var algorithmRelationType = getCreatedAlgorithmRelationType("algorithmRelationTypeName");
 
-    @Test
-    void createAlgorithmRelation_TypeNotFound() {
+        AlgorithmRelation algorithmRelation = buildAlgorithmRelation(
+                sourceAlgorithm, targetAlgorithm, algorithmRelationType, "description");
 
+        var persistedAlgorithmRelation = algorithmRelationService.create(algorithmRelation);
+
+        assertAlgorithmRelationEquality(persistedAlgorithmRelation, algorithmRelation);
     }
 
     @Test
     void createAlgorithmRelation_AlgorithmNotFound() {
+        Algorithm sourceAlgorithm = new ClassicAlgorithm();
+        sourceAlgorithm.setName("sourceAlgorithmName");
+        sourceAlgorithm.setId(UUID.randomUUID());
+        Algorithm targetAlgorithm = new ClassicAlgorithm();
+        targetAlgorithm.setName("targetAlgorithmName");
+        targetAlgorithm.setId(UUID.randomUUID());
 
+        var algorithmRelationType = getCreatedAlgorithmRelationType("algorithmRelationTypeName");
+
+        AlgorithmRelation algorithmRelation = buildAlgorithmRelation(
+                sourceAlgorithm, targetAlgorithm, algorithmRelationType, "description");
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> algorithmRelationService.create(algorithmRelation));
     }
 
     @Test
     void findAlgorithmRelationById_ElementFound() {
+        Algorithm sourceAlgorithm = getCreatedAlgorithm("sourceAlgorithmName");
+        Algorithm targetAlgorithm = getCreatedAlgorithm("targetAlgorithmName");
 
+        var algorithmRelationType = getCreatedAlgorithmRelationType("algorithmRelationTypeName");
+
+        AlgorithmRelation algorithmRelation = buildAlgorithmRelation(
+                sourceAlgorithm, targetAlgorithm, algorithmRelationType, "description");
+
+        var persistedAlgorithmRelation = algorithmRelationService.create(algorithmRelation);
+
+        persistedAlgorithmRelation = algorithmRelationService.findById(persistedAlgorithmRelation.getId());
+
+        assertAlgorithmRelationEquality(persistedAlgorithmRelation, algorithmRelation);
     }
 
     @Test
     void findAlgorithmRelationById_ElementNotFound() {
-
+        Assertions.assertThrows(NoSuchElementException.class, () -> algorithmRelationService.findById(UUID.randomUUID()));
     }
 
     @Test
     void updateAlgorithmRelation_ElementFound() {
+        Algorithm sourceAlgorithm = getCreatedAlgorithm("sourceAlgorithmName");
+        Algorithm targetAlgorithm = getCreatedAlgorithm("targetAlgorithmName");
 
+        var algorithmRelationType = getCreatedAlgorithmRelationType("algorithmRelationTypeName");
+
+        AlgorithmRelation algorithmRelation = buildAlgorithmRelation(
+                sourceAlgorithm, targetAlgorithm, algorithmRelationType, "description");
+        AlgorithmRelation compareAlgorithmRelation = buildAlgorithmRelation(
+                sourceAlgorithm, targetAlgorithm, algorithmRelationType, "description");
+
+        var persistedAlgorithmRelation = algorithmRelationService.create(algorithmRelation);
+        compareAlgorithmRelation.setId(persistedAlgorithmRelation.getId());
+
+        String editDescription = "editedDescription";
+        var editedType = getCreatedAlgorithmRelationType("editedAlgorithmRelationTypeName");
+        persistedAlgorithmRelation.setDescription(editDescription);
+        persistedAlgorithmRelation.setAlgorithmRelationType(editedType);
+
+        var updatedAlgorithmRelation = algorithmRelationService.update(persistedAlgorithmRelation);
+
+        assertThat(updatedAlgorithmRelation.getId()).isNotNull();
+        assertThat(updatedAlgorithmRelation.getId()).isEqualTo(compareAlgorithmRelation.getId());
+        assertThat(updatedAlgorithmRelation.getDescription()).isNotEqualTo(compareAlgorithmRelation.getDescription());
+        assertThat(updatedAlgorithmRelation.getDescription()).isEqualTo(editDescription);
+        assertThat(updatedAlgorithmRelation.getAlgorithmRelationType().getId())
+                .isNotEqualTo(compareAlgorithmRelation.getAlgorithmRelationType().getId());
+        assertThat(updatedAlgorithmRelation.getAlgorithmRelationType().getId())
+                .isEqualTo(editedType.getId());
+        assertThat(updatedAlgorithmRelation.getAlgorithmRelationType().getName())
+                .isNotEqualTo(compareAlgorithmRelation.getAlgorithmRelationType().getName());
+        assertThat(updatedAlgorithmRelation.getAlgorithmRelationType().getName())
+                .isEqualTo(editedType.getName());
+        assertThat(updatedAlgorithmRelation.getSourceAlgorithm().getId()).isEqualTo(compareAlgorithmRelation.getSourceAlgorithm().getId());
+        assertThat(updatedAlgorithmRelation.getTargetAlgorithm().getId()).isEqualTo(compareAlgorithmRelation.getTargetAlgorithm().getId());
     }
 
     @Test
     void updateAlgorithmRelation_ElementNotFound() {
-
+        AlgorithmRelation algorithmRelation = buildAlgorithmRelation(
+                null, null, null, "description");
+        algorithmRelation.setId(UUID.randomUUID());
+        Assertions.assertThrows(NoSuchElementException.class, () -> algorithmRelationService.update(algorithmRelation));
     }
 
     @Test
     void deleteAlgorithmRelation_ElementFound() {
+        Algorithm sourceAlgorithm = getCreatedAlgorithm("sourceAlgorithmName");
+        Algorithm targetAlgorithm = getCreatedAlgorithm("targetAlgorithmName");
 
+        var algorithmRelationType = getCreatedAlgorithmRelationType("algorithmRelationTypeName");
+
+        AlgorithmRelation algorithmRelation = buildAlgorithmRelation(
+                sourceAlgorithm, targetAlgorithm, algorithmRelationType, "description");
+
+        var persistedAlgorithmRelation = algorithmRelationService.create(algorithmRelation);
+
+        Assertions.assertDoesNotThrow(() -> algorithmRelationService.findById(persistedAlgorithmRelation.getId()));
+
+        algorithmRelationService.delete(persistedAlgorithmRelation.getId());
+
+        Assertions.assertThrows(NoSuchElementException.class, () ->
+                algorithmRelationService.findById(persistedAlgorithmRelation.getId()));
     }
 
     @Test
     void deleteAlgorithmRelation_ElementNotFound() {
+        Assertions.assertThrows(NoSuchElementException.class, () ->
+                algorithmRelationService.delete(UUID.randomUUID()));
+    }
 
+    private void assertAlgorithmRelationEquality(AlgorithmRelation persistedRelation, AlgorithmRelation relation) {
+        assertThat(persistedRelation.getId()).isNotNull();
+        assertThat(persistedRelation.getDescription()).isEqualTo(relation.getDescription());
+        assertThat(persistedRelation.getAlgorithmRelationType().getId())
+                .isEqualTo(relation.getAlgorithmRelationType().getId());
+        assertThat(persistedRelation.getAlgorithmRelationType().getName())
+                .isEqualTo(relation.getAlgorithmRelationType().getName());
+        assertThat(persistedRelation.getSourceAlgorithm().getId()).isEqualTo(relation.getSourceAlgorithm().getId());
+        assertThat(persistedRelation.getTargetAlgorithm().getId()).isEqualTo(relation.getTargetAlgorithm().getId());
+    }
+
+    private AlgorithmRelation buildAlgorithmRelation (
+            Algorithm source, Algorithm target, AlgorithmRelationType type, String description) {
+        AlgorithmRelation algorithmRelation = new AlgorithmRelation();
+        algorithmRelation.setDescription(description);
+        algorithmRelation.setSourceAlgorithm(source);
+        algorithmRelation.setTargetAlgorithm(target);
+        algorithmRelation.setAlgorithmRelationType(type);
+        return algorithmRelation;
+    }
+
+    private Algorithm getCreatedAlgorithm(String name) {
+        Algorithm algorithm = new ClassicAlgorithm();
+        algorithm.setName(name);
+        return algorithmService.create(algorithm);
+    }
+
+    private AlgorithmRelationType getCreatedAlgorithmRelationType(String name) {
+        AlgorithmRelationType algorithmRelationType = new AlgorithmRelationType();
+        algorithmRelationType.setName(name);
+        return algorithmRelationTypeService.create(algorithmRelationType);
     }
 }
