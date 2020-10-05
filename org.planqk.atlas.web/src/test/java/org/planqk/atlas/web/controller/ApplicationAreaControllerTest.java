736a7dd445b019be38d7c9e03690ec22df0c2d4f
Christian Müller <dev@c-mueller.xyz>
2020-10-05 02:04:12 +0200

    Add AAControllerTest

--- org.planqk.atlas.web/src/test/java/org/planqk/atlas/web/controller/ApplicationAreaControllerTest.java
+++ org.planqk.atlas.web/src/test/java/org/planqk/atlas/web/controller/ApplicationAreaControllerTest.java
@@ -21,11 +21,11 @@ package org.planqk.atlas.web.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.UUID;
 
 import org.planqk.atlas.core.model.ApplicationArea;
 import org.planqk.atlas.core.services.ApplicationAreaService;
-import org.planqk.atlas.core.services.LinkingService;
 import org.planqk.atlas.web.controller.util.ObjectMapperUtils;
 import org.planqk.atlas.web.dtos.ApplicationAreaDto;
 import org.planqk.atlas.web.linkassembler.EnableLinkAssemblers;
@@ -49,10 +49,14 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-import static org.springframework.http.RequestEntity.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -65,8 +69,6 @@ public class ApplicationAreaControllerTest {
 
     @MockBean
     private ApplicationAreaService applicationAreaService;
-    @MockBean
-    private LinkingService linkingService;
 
     @Autowired
     private MockMvc mockMvc;
@@ -136,22 +138,166 @@ public class ApplicationAreaControllerTest {
 
         doReturn(area).when(applicationAreaService).create(any());
 
-        MvcResult result = mockMvc
-                .perform(
-                        post(
-                                linkBuilderService.urlStringTo(
-                                        methodOn(ApplicationAreaController.class)
-                                                .getApplicationAreas(new ListParameters(PageRequest.of(1, 10), "hellp"))
-                                )
-                        ).accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$._embedded.applicationAreas[0].id").value(area.getId().toString()))
-                .andExpect(jsonPath("$._embedded.applicationAreas[0].name").value(area.getName()))
-                .andReturn();
+        mockMvc.perform(
+                post(
+                        linkBuilderService.urlStringTo(
+                                methodOn(ApplicationAreaController.class)
+                                        .createApplicationArea(null)
+                        )
+                ).accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(areaDto))
+        ).andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(area.getId().toString()))
+                .andExpect(jsonPath("$.name").value(area.getName()));
+    }
 
-        var page = ObjectMapperUtils.getPageInfo(result.getResponse().getContentAsString());
+    @Test
+    @SuppressWarnings("ConstantConditions")
+    public void createApplicationArea_returnBadRequest() throws Exception {
+        var areaDto = new ApplicationAreaDto();
+        areaDto.setName(null);
 
-        assertThat(page.getSize()).isEqualTo(1);
-        assertThat(page.getNumber()).isEqualTo(0);
+        mockMvc.perform(
+                post(
+                        linkBuilderService.urlStringTo(
+                                methodOn(ApplicationAreaController.class)
+                                        .createApplicationArea(null)
+                        )
+                ).accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(areaDto))
+        ).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @SuppressWarnings("ConstantConditions")
+    public void updateApplicationArea_returnCreated() throws Exception {
+        var areaDto = new ApplicationAreaDto();
+        areaDto.setName("test");
+
+        var area = new ApplicationArea();
+        area.setName(areaDto.getName());
+        area.setId(UUID.randomUUID());
+
+        doReturn(area).when(applicationAreaService).update(any());
+
+        mockMvc.perform(
+                put(
+                        linkBuilderService.urlStringTo(
+                                methodOn(ApplicationAreaController.class)
+                                        .updateApplicationArea(area.getId(), null)
+                        )
+                ).accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(areaDto))
+        ).andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(area.getId().toString()))
+                .andExpect(jsonPath("$.name").value(area.getName()));
+    }
+
+    @Test
+    @SuppressWarnings("ConstantConditions")
+    public void updateApplicationArea_InvalidBody_returnBadRequest() throws Exception {
+        var areaDto = new ApplicationAreaDto();
+        areaDto.setId(UUID.randomUUID());
+        areaDto.setName(null);
+        mockMvc.perform(
+                put(
+                        linkBuilderService.urlStringTo(
+                                methodOn(ApplicationAreaController.class)
+                                        .updateApplicationArea(areaDto.getId(), null)
+                        )
+                ).accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(areaDto))
+        ).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @SuppressWarnings("ConstantConditions")
+    public void updateApplicationArea_returnNotFound() throws Exception {
+        var areaDto = new ApplicationAreaDto();
+        areaDto.setId(UUID.randomUUID());
+        areaDto.setName("test");
+
+        doThrow(new NoSuchElementException()).when(applicationAreaService).update(any());
+
+        mockMvc.perform(
+                put(
+                        linkBuilderService.urlStringTo(
+                                methodOn(ApplicationAreaController.class)
+                                        .updateApplicationArea(areaDto.getId(), null)
+                        )
+                ).accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(areaDto))
+        ).andExpect(status().isNotFound());
+    }
+
+    @Test
+    @SuppressWarnings("ConstantConditions")
+    public void deleteApplicationArea_returnNoContent() throws Exception {
+        doNothing().when(applicationAreaService).delete(any());
+
+        mockMvc.perform(
+                delete(
+                        linkBuilderService.urlStringTo(
+                                methodOn(ApplicationAreaController.class)
+                                        .deleteApplicationArea(UUID.randomUUID())
+                        )
+                ).accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isNoContent());
+    }
+
+    @Test
+    @SuppressWarnings("ConstantConditions")
+    public void deleteApplicationArea_returnNotFound() throws Exception {
+        doThrow(new NoSuchElementException()).when(applicationAreaService).delete(any());
+
+        mockMvc.perform(
+                delete(
+                        linkBuilderService.urlStringTo(
+                                methodOn(ApplicationAreaController.class)
+                                        .deleteApplicationArea(UUID.randomUUID())
+                        )
+                ).accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isNotFound());
+    }
+
+    @Test
+    @SuppressWarnings("ConstantConditions")
+    public void getApplicationArea_returnOk() throws Exception {
+        var area = new ApplicationArea();
+        area.setId(UUID.randomUUID());
+        area.setName("test");
+
+        doReturn(area).when(applicationAreaService).findById(any());
+
+        mockMvc.perform(
+                get(
+                        linkBuilderService.urlStringTo(
+                                methodOn(ApplicationAreaController.class)
+                                        .getApplicationArea(UUID.randomUUID())
+                        )
+                ).accept(MediaType.APPLICATION_JSON)
+        ).andExpect(jsonPath("$.id").value(area.getId().toString()))
+                .andExpect(jsonPath("$.name").value(area.getName()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @SuppressWarnings("ConstantConditions")
+    public void getApplicationArea_returnNotFound() throws Exception {
+        doThrow(new NoSuchElementException()).when(applicationAreaService).delete(any());
+
+        mockMvc.perform(
+                delete(
+                        linkBuilderService.urlStringTo(
+                                methodOn(ApplicationAreaController.class)
+                                        .deleteApplicationArea(UUID.randomUUID())
+                        )
+                ).accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isNotFound());
     }
 }
