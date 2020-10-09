b83e4e090114ce6199a00c077c91e562293dfa22
radufalcusanStoneone <66017695+radufalcusanStoneone@users.noreply.github.com>
2020-10-09 09:53:44 +0200

    add getImplementation() in ImplementationGlobalController.java (#141)
    * add getImplementation() in ImplementationGlobalController.java
    
    * delete unused mocks after code review

--- org.planqk.atlas.web/src/main/java/org/planqk/atlas/web/controller/ImplementationGlobalController.java
+++ org.planqk.atlas.web/src/main/java/org/planqk/atlas/web/controller/ImplementationGlobalController.java
@@ -19,6 +19,8 @@
 
 package org.planqk.atlas.web.controller;
 
+import java.util.UUID;
+
 import org.planqk.atlas.core.services.ImplementationService;
 import org.planqk.atlas.web.Constants;
 import org.planqk.atlas.web.annotation.ApiVersion;
@@ -26,6 +28,14 @@ import org.planqk.atlas.web.dtos.ImplementationDto;
 import org.planqk.atlas.web.linkassembler.ImplementationAssembler;
 import org.planqk.atlas.web.utils.ListParameters;
 import org.planqk.atlas.web.utils.ListParametersDoc;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -33,13 +43,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.PagedModel;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Controller to access implementations outside of the context of its implemented algorithm.
@@ -66,5 +69,17 @@ public class ImplementationGlobalController {
             @Parameter(hidden = true) ListParameters listParameters) {
         var implementations = implementationService.findAll(listParameters.getPageable());
         return ResponseEntity.ok(implementationAssembler.toModel(implementations));
+    }
+
+    @Operation(responses = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400"),
+            @ApiResponse(responseCode = "404", description = "Implementation with given ID doesn't exist")
+    }, description = "Retrieve a specific implementation and its basic properties.")
+    @GetMapping("/{implementationId}")
+    public ResponseEntity<EntityModel<ImplementationDto>> getImplementation(
+            @PathVariable UUID implementationId) {
+        var implementation = this.implementationService.findById(implementationId);
+        return ResponseEntity.ok(implementationAssembler.toModel(implementation));
     }
 }
---
+++ org.planqk.atlas.web/src/test/java/org/planqk/atlas/web/controller/ImplementationGlobalControllerTest.java
@@ -0,0 +1,80 @@
+package org.planqk.atlas.web.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.planqk.atlas.core.model.Algorithm;
+import org.planqk.atlas.core.model.Implementation;
+import org.planqk.atlas.core.services.ImplementationService;
+import org.planqk.atlas.web.controller.util.ObjectMapperUtils;
+import org.planqk.atlas.web.dtos.ImplementationDto;
+import org.planqk.atlas.web.linkassembler.EnableLinkAssemblers;
+import org.planqk.atlas.web.linkassembler.LinkBuilderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebMvcTest(controllers = ImplementationGlobalController.class)
+@ExtendWith(MockitoExtension.class)
+@AutoConfigureMockMvc
+@EnableLinkAssemblers
+public class ImplementationGlobalControllerTest {
+
+    private final ObjectMapper mapper = ObjectMapperUtils.newTestMapper();
+
+    @MockBean
+    private ImplementationService implementationService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private LinkBuilderService linkBuilderService;
+
+    private Implementation implementation1;
+
+    @Test
+    public void getImplementation() throws Exception {
+        this.implementation1 = mockValidMinimalImpl(UUID.randomUUID());
+
+        when(implementationService.findById(this.implementation1.getId())).thenReturn(this.implementation1);
+
+        var url = linkBuilderService.urlStringTo(methodOn(ImplementationGlobalController.class)
+                .getImplementation(implementation1.getId()));
+
+        MvcResult result = mockMvc.perform(get(url).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andReturn();
+
+        EntityModel<ImplementationDto> response = mapper.readValue(result.getResponse().getContentAsString(),
+                new TypeReference<>() {
+                });
+        assertEquals(response.getContent().getId(), implementation1.getId());
+    }
+
+    private Implementation mockValidMinimalImpl(UUID implId) {
+        Algorithm algo = new Algorithm();
+        algo.setName("dummy");
+        Implementation implementation = new Implementation();
+        implementation.setName("implementation for Shor");
+        implementation.setId(implId);
+        implementation.setImplementedAlgorithm(algo);
+        return implementation;
+    }
+
+}
