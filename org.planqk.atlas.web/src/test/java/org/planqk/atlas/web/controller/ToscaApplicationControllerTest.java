d8468e02fc5961da5c1098c0898578c064d3819c
Marvin Bechtold <marvin.bechtold.dev@gmail.com>
2022-02-16 16:42:28 +0100

    add delete and get tests
    Signed-off-by: Marvin Bechtold <marvin.bechtold.dev@gmail.com>

--- org.planqk.atlas.web/src/test/java/org/planqk/atlas/web/controller/ToscaApplicationControllerTest.java
+++ org.planqk.atlas.web/src/test/java/org/planqk/atlas/web/controller/ToscaApplicationControllerTest.java
@@ -19,42 +19,45 @@
 
 package org.planqk.atlas.web.controller;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.planqk.atlas.core.model.ComputeResource;
 import org.planqk.atlas.core.model.ToscaApplication;
 import org.planqk.atlas.core.services.ToscaApplicationService;
 import org.planqk.atlas.web.controller.util.ObjectMapperUtils;
-import org.planqk.atlas.web.dtos.ComputeResourceDto;
 import org.planqk.atlas.web.dtos.ToscaApplicationDto;
 import org.planqk.atlas.web.linkassembler.EnableLinkAssemblers;
 import org.planqk.atlas.web.linkassembler.LinkBuilderService;
+import org.planqk.atlas.web.utils.ListParameters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
-import org.springframework.http.codec.multipart.Part;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.mock.web.MockPart;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
+import org.springframework.test.web.servlet.MvcResult;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -83,7 +86,8 @@ class ToscaApplicationControllerTest {
     private LinkBuilderService linkBuilderService;
 
     @Test
-    public void addToscaApplication_returnCreated() throws Exception{
+    @SneakyThrows
+    public void createApplication_returnCreated() {
 
         UUID uuid = UUID.randomUUID();
         String name = "Test Name";
@@ -104,20 +108,80 @@ class ToscaApplicationControllerTest {
         MockPart namePart = new MockPart("name", "name", name.getBytes());
         namePart.getHeaders().setContentType(MediaType.TEXT_PLAIN);
 
-
         mockMvc.perform(
-                multipart(
-                        linkBuilderService.urlStringTo(
-                                methodOn(ToscaApplicationController.class).createApplication(null,null)
-                        )
-                ).file(file).part(namePart)
-        ).andExpect(status().isCreated())
+                        multipart(
+                                linkBuilderService.urlStringTo(
+                                        methodOn(ToscaApplicationController.class).createApplication(null, null)
+                                )
+                        ).file(file).part(namePart)
+                ).andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id").value(uuid.toString()))
-                .andExpect(jsonPath("$.name").value(name));;
+                .andExpect(jsonPath("$.name").value(name));
     }
 
     @Test
-    public void updateToscaApplication_returnNotFound() throws Exception {
+    @SneakyThrows
+    void getApplications_EmptyList_returnOk() {
+        doReturn(new PageImpl<ToscaApplication>(List.of())).when(toscaApplicationService).findAll(any());
+
+        var url = linkBuilderService.urlStringTo(methodOn(ToscaApplicationController.class)
+                .getApplications(ListParameters.getDefault()));
+        MvcResult mvcResult = mockMvc
+                .perform(get(url).accept(APPLICATION_JSON))
+                .andExpect(status().isOk()).andReturn();
+        assertEquals(ObjectMapperUtils.mapResponseToList(mvcResult, ToscaApplicationDto.class).size(), 0);
+    }
+
+    @Test
+    @SneakyThrows
+    void getApplications_SingleElement_returnOk() {
+        var toscaApplication = new ToscaApplication();
+        toscaApplication.setId(UUID.randomUUID());
+        toscaApplication.setName("name");
+        toscaApplication.setToscaName("toscaName");
+        toscaApplication.setToscaID("toscaID");
+        toscaApplication.setToscaNamespace("toscaNameSpace");
+
+        doReturn(new PageImpl<>(List.of(toscaApplication))).when(toscaApplicationService).findAll(any());
+
+        var url = linkBuilderService.urlStringTo(methodOn(ToscaApplicationController.class)
+                .getApplications(ListParameters.getDefault()));
+        MvcResult mvcResult = mockMvc.perform(get(url).accept(APPLICATION_JSON))
+                .andExpect(status().isOk()).andReturn();
+        ToscaApplicationDto toscaApplicationDto = ObjectMapperUtils.mapResponseToList(mvcResult, ToscaApplicationDto.class).get(0);
+        assertEquals(toscaApplicationDto.getId(), toscaApplication.getId());
+        assertEquals(toscaApplicationDto.getToscaID(), toscaApplication.getToscaID());
+        assertEquals(toscaApplicationDto.getToscaNamespace(), toscaApplication.getToscaNamespace());
+        assertEquals(toscaApplicationDto.getToscaName(), toscaApplication.getToscaName());
+    }
+
+    @Test
+    @SneakyThrows
+    void getApplication_returnOk() {
+        var toscaApplication = new ToscaApplication();
+        toscaApplication.setId(UUID.randomUUID());
+        toscaApplication.setName("name");
+        toscaApplication.setToscaName("toscaName");
+        toscaApplication.setToscaID("toscaID");
+        toscaApplication.setToscaNamespace("toscaNameSpace");
+
+        doReturn(toscaApplication).when(toscaApplicationService).findById(any());
+
+        var url = linkBuilderService.urlStringTo(methodOn(ToscaApplicationController.class)
+                .getApplication(toscaApplication.getId()));
+
+        mockMvc.perform(get(url).accept(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(toscaApplication.getId().toString()))
+                .andExpect(jsonPath("$.name").value(toscaApplication.getName()))
+                .andExpect(jsonPath("$.toscaName").value(toscaApplication.getToscaName()))
+                .andExpect(jsonPath("$.toscaID").value(toscaApplication.getToscaID()))
+                .andExpect(jsonPath("$.toscaNamespace").value(toscaApplication.getToscaNamespace()));
+    }
+
+    @Test
+    @SneakyThrows
+    public void updateApplication_returnNotFound() {
         var resource = new ToscaApplicationDto();
         resource.setId(UUID.randomUUID());
         resource.setName("Hello World");
@@ -136,10 +200,10 @@ class ToscaApplicationControllerTest {
     }
 
     @Test
-    public void updateToscaApplication_returnBadRequest() throws Exception {
+    @SneakyThrows
+    public void updateApplication_returnBadRequest() {
         var resource = new ToscaApplicationDto();
         resource.setId(UUID.randomUUID());
-        resource.setName("Hello World");
 
         mockMvc.perform(
                 put(
@@ -153,7 +217,8 @@ class ToscaApplicationControllerTest {
     }
 
     @Test
-    public void updateToscaApplication_returnOk() throws Exception {
+    @SneakyThrows
+    public void updateApplication_returnOk() {
         var resource = new ToscaApplicationDto();
         resource.setId(UUID.randomUUID());
         resource.setName("Hello World");
@@ -178,5 +243,16 @@ class ToscaApplicationControllerTest {
                 .andExpect(jsonPath("$.name").value(resource.getName()));
     }
 
-
+    @Test
+    @SneakyThrows
+    void deleteApplication_returnNoContent() {
+        doNothing().when(toscaApplicationService).delete(any());
+        mockMvc.perform(
+                delete(
+                        linkBuilderService.urlStringTo(
+                                methodOn(ToscaApplicationController.class).deleteApplication(UUID.randomUUID())
+                        )
+                ).accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isNoContent());
+    }
 }
