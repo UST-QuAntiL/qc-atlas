a3439caa92fa0bd5a9030656e8392ada81fae4af
Philipp Wundrack <philipp.wundrack@live.de>
2024-05-15 16:15:27 +0200

    add solution data model and endpoints

---
+++ org.planqk.atlas.core/src/main/java/org/planqk/atlas/core/model/Solution.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2024 the qc-atlas contributors.
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package org.planqk.atlas.core.model;
+
+import java.util.UUID;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToOne;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+/**
+ * Entity representing a solution.
+ */
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Entity
+public class Solution extends KnowledgeArtifact {
+
+    private UUID patternId;
+
+    @OneToOne(fetch = FetchType.LAZY, orphanRemoval = true)
+    @EqualsAndHashCode.Exclude
+    @JoinTable(
+            name = "SolutionFile",
+            joinColumns = @JoinColumn(name = "solution_id"),
+            inverseJoinColumns = @JoinColumn(name = "file_id")
+    )
+    private File file;
+}
